cd1e95840121004eec386dc2942f364c67b533b2
alimansi82 <alimansi1382@gmail.com>
2022-01-31 00:40:23 -0800

    add path file and const variable

---
+++ __PATH__.php
@@ -0,0 +1,9 @@
+<?php
+
+const TEMPLATE_PATH = "public/templates/";
+const VIEW_PATH = "public/views/";
+const ASSETS_PATH = "assets/";
+const STYLE_PATH = "assets/css/";
+const SCRIPT_PATH = "assets/js/";
+const PLUGIN_PATH = "plugin/";
+const ADMIN_VIEW_PATH = "public/Admin/views/";
